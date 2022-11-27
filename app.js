d5def1bedf8b44d8fe0edcab923118c0444b6f9d
samnb175 <samirnabee@hotmail.com>
2022-11-27 21:08:35 +0400

    dark mode

--- app.js
+++ app.js
@@ -8,4 +8,40 @@ const typing = () => {
     }
 }
 
+const darkModeBtn = document.getElementById('darkMode')
+const containerEle = document.querySelector('.container')
+const cardEle = document.querySelectorAll('.card')
+const profileImgEle = document.querySelectorAll('.profile-image')
+const jobEle = document.querySelectorAll('.job')
+const aboutMeEle = document.querySelectorAll('.about-me')
+
+
+
+darkModeBtn.addEventListener('click', () => {
+
+    if (darkModeBtn.classList.contains('far')) {
+        darkModeBtn.classList.remove('far')
+        darkModeBtn.classList.add('fas')
+    } else {
+        darkModeBtn.classList.add('far')
+        darkModeBtn.classList.remove('fas')
+    }
+
+    containerEle.classList.toggle('dark')
+    cardEle.forEach(element => {
+        element.classList.toggle('card-dark') 
+    });
+    jobEle.forEach(element => {
+        element.classList.toggle('job-dark') 
+    });
+    profileImgEle.forEach(element => {
+        element.classList.toggle('profile-image-dark') 
+    });
+    aboutMeEle.forEach(element => {
+        element.classList.toggle('about-me-dark') 
+    });
+
+
+})
+
 typing()
